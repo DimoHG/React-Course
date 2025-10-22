f2504f1ecbd85112a9ece577ea3adaea0b374bf4
Dimo Georgiev <dimo.georgiev@outlook.com>
2025-10-22 19:20:01 +0300

    S2: Finished

---
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -0,0 +1,7 @@
+// Create an array with numbers from 1 to 100
+const numbers = Array.from({ length: 100 }, (_, index) => index + 1);
+
+// Print each number to the console
+numbers.forEach(number => {
+  console.log(number);
+});
