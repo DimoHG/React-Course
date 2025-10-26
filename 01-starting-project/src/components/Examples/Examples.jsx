cb7f9b924ec35fbd0f8c970f6c782a1c29490e07
Dimo Georgiev <dimo.georgiev@outlook.com>
2025-10-26 14:16:10 +0200

    S4: Extract Tabs into a component

--- 01-starting-project/src/components/Examples/Examples.jsx
+++ 01-starting-project/src/components/Examples/Examples.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 
 import TabButton from '../TabButton/TabButton.jsx'
 import { EXAMPLES } from '../../data.js'
-import Section from '../Section.jsx';
+import Section from '../Section/Section.jsx';
+import Tabs from '../Tabs/Tabs.jsx';
 
 export default function Examples() {
     const [selectedTopic, setSelectedTopic] = useState();
@@ -26,12 +27,13 @@ export default function Examples() {
 
     return (
         <Section title="Examples" id="examples">
-            <menu>
-                <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')} >Components</TabButton>
-                <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')} >JSX</TabButton>
-                <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')} >Props</TabButton>
-                <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton>
-            </menu>
-            {tabContent}
+            <Tabs buttons={
+                <>
+                    <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')} >Components</TabButton>
+                    <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')} >JSX</TabButton>
+                    <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')} >Props</TabButton>
+                    <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton>
+                </>
+            }>{tabContent}</Tabs>
         </Section>)
 }
