1b0d54acae0356bc4f88e22fab03b8c1dfbcdb98
Dimo Georgiev <dimo.georgiev@outlook.com>
2025-10-29 11:05:43 +0200

    S4: Implement edit player name

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,31 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react"
+
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        // setIsEditing(!isEditing);  --> Unfortunately, not a good practice in React
+        setIsEditing(editing => !editing); // --> The correct way
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let playerNameLine = <span className='player-name'>{playerName}</span>;
+    if (isEditing) {
+        playerNameLine = <input type="text" value={playerName} required onChange={handleChange}></input>
+
+    }
+
     return (
         <li>
             <span className='player'>
-                <span className='player-name'>{name}</span>
+                {playerNameLine}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     )
 }
--- Notes.md
+++ Notes.md
@@ -1,2 +1,3 @@
-#Initial Commit
-2
+# Notes
+
+> React is scheduling state updates. Don't pass (!isEditing) to setIsEditing, as it might not work properly. Always pass a function to the setIsEditing if you want to reverse the state.
