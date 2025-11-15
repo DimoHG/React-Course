dfe191c0fff83bbd13ca7f10120710ec17906b37
DimoHG <dimo.georgiev@redis.com>
2025-11-15 22:05:19 +0200

    S4: Lift state up

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -13,11 +13,22 @@ function App() {
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare}></GameBoard>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        ></GameBoard>
       </div>
       LOG
     </main>
--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,31 +1,37 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
-    const [playerName, setPlayerName] = useState(initialName);
-    const [isEditing, setIsEditing] = useState(false);
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
-    function handleEditClick() {
-        // setIsEditing(!isEditing);  --> Unfortunately, not a good practice in React
-        setIsEditing(editing => !editing); // --> The correct way
-    }
+  function handleEditClick() {
+    // setIsEditing(!isEditing);  --> Unfortunately, not a good practice in React
+    setIsEditing((editing) => !editing); // --> The correct way
+  }
 
-    function handleChange(event) {
-        setPlayerName(event.target.value);
-    }
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
 
-    let playerNameLine = <span className='player-name'>{playerName}</span>;
-    if (isEditing) {
-        playerNameLine = <input type="text" value={playerName} required onChange={handleChange}></input>
+  let playerNameLine = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    playerNameLine = (
+      <input
+        type="text"
+        value={playerName}
+        required
+        onChange={handleChange}
+      ></input>
+    );
+  }
 
-    }
-
-    return (
-        <li>
-            <span className='player'>
-                {playerNameLine}
-                <span className="player-symbol">{symbol}</span>
-            </span>
-            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
-        </li>
-    )
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {playerNameLine}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
 }
